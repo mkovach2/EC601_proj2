fc93557a968adbda5267229bb4c4781a5cf1ea11
unknown <mkovach@bu.edu>
2020-09-28 12:44:11 -0400

    changed dashes do underscores in filenames, spyder does not like those

---
+++ comnbined.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+callingTNLP.py
+Author: Miles Kovach
+Created: 2020-09-27
+Modified: 2020-09-27
+EC601 Assignment 2
+
+This script uses bits from each of the other modules, test_of_googleNLP
+and test_of_tweepy.
+'''
+
+from test_of_tweepy import myOwnLast
+from test_of_googleNLP import text2doc
+from test_of_googleNLP import analSent
+
+
