ec02d5f6077a2a76cac780b6c5046dbc4fd7fb86
unknown <mkovach@bu.edu>
2020-09-28 15:30:53 -0400

    removed duplicates added by fetch

--- comnbined.py
+++ comnbined.py
@@ -36,44 +36,4 @@ def main():
     print(allUsers[i], '\t\t', allSentiments[i])
   
 if __name__ == '__main__':
-=======
-# -*- coding: utf-8 -*-
-'''
-comnbined.py
-Author: Miles Kovach
-Created: 2020-09-27
-Modified: 2020-09-28
-EC601 Assignment 2
-
-This script uses bits from each of the other modules, test_of_googleNLP
-and test_of_tweepy.
-'''
-
-debug_mode = []
-
-import test_of_tweepy as tot
-import test_of_googleNLP as gnlp
-
-# debug block 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-lastTweet = tot.myOwnLast(10)
-allUsers = []
-allTweets = []
-allSentiments = []
-
-if 1 in debug_mode:
-  for tweetItem in lastTweet:
-    print(tweetItem.text)
-
-for tweetItem in lastTweet:
-  allUsers.append(tweetItem.author.screen_name)
-  allTweets.append(tweetItem.text)
-  allSentiments.append(gnlp.analSent(tweetItem.text))
-  
-def main():
-  for i in range(len(allTweets)):
-    print(allUsers[i], '\t\t', allSentiments[i])
-  
-if __name__ == '__main__':
->>>>>>> 484f19e39b931ca2e023fa908173b63f27a4b620
   main()
--- test_of_tweepy.py
+++ test_of_tweepy.py
@@ -39,47 +39,4 @@ def main():
   myOwnLast(19)
 
 if __name__ == '__main__':
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 25 11:10:06 2020
-
-@author: miles_kovach
-"""
-
-import tweepy
-import configparser
-
-confKeys = configparser.ConfigParser(interpolation=None)
-
-debug_mode = []
-
-# reading keys from authentication from an authentication file called
-# important.ini.  see iniTemplate.txt for how to format this.
-confKeys.read('important.ini')
-keyDict = confKeys['authentication_info']
-
-auth = tweepy.OAuthHandler(keyDict['api_key'], keyDict['api_secret'])
-auth.set_access_token(keyDict['access_token'], keyDict['access_secret'])
-
-api = tweepy.API(auth)
-
-def myOwnLast(numTweets): # function that prints the most recent numTweet...
-                          # ... tweets from one's own feed
-  public_tweets = api.home_timeline()
-  if 1 in debug_mode:
-    for tweet in public_tweets[:numTweets]:
-      print(tweet.author.screen_name, '\t', tweet.created_at, '\n', tweet.text, '\n')
-    
-  return public_tweets[:numTweets]
-    
-def searchShortcut(term, num):
- # j = api.search(q = 'term', )
-  j = 1
- 
-def main():
-  myOwnLast(19)
-
-if __name__ == '__main__':
->>>>>>> 484f19e39b931ca2e023fa908173b63f27a4b620
   main()
